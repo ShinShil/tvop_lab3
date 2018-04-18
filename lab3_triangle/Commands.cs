1875c4d022e2d602e8cddd61327c0d73cbad1a6c
ShinShil <starostin.450503@gmail.com>
2018-04-19 01:49:13 +0300

    print menu

---
+++ lab3_triangle/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_triangle
+{
+    class Commands
+    {
+        public static void ExecuteCommand(int command)
+        {
+
+        }
+    }
+}
---
+++ lab3_triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int command;
+            do
+            {
+                Menu.Print();
+                command = Menu.Read();
+                Commands.ExecuteCommand(command);
+            } while (command != 2);
+        }
+    }
+}
