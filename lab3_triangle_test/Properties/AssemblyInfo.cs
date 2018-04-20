1c54d458628358c63287cd9b600188467dea1fd1
ShinShil <starostin.450503@gmail.com>
2018-04-20 12:12:05 +0300

    prepare for unit tests

---
+++ lab3_triangle_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("lab3_triangle_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab3_triangle_test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b4af71a-4506-4125-bc3b-ce79ae209d4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lab3_triangle_test_nunit/TriangleModel.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_triangle_test_nunit
+{
+    [TestFixture]
+    public class TriangleModel
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.True(true);
+        }
+    }
+}
